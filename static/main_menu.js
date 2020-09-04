e2636c76ca9a44242af59a740f2d08c099f0c2bf
jitetbltd <70622279+jitetbltd@users.noreply.github.com>
2020-09-04 16:28:59 +0600

    Update main_menu.js

--- static/main_menu.js
+++ static/main_menu.js
@@ -1,8 +1,9 @@
 var main_menu = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
-        <a class="navbar-brand mx-auto" href="#">
-            <h2 style="font-family: Arial Black;">JIT ETB LTD</h2>
+        <a class="navbar-brand mx-auto" href="./index.html" style="font-family: Arial Black;">
+            <img src="./static/logo/logo_gr.jpg" width="34" height="34" class="d-inline-block align-top" alt="JITETBLTD" loading="lazy" />
+            JIT ETB LTD
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
